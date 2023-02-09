9faedae9fd209d5f60708df9b16842452638e1d1
maggi86 <meaganwilliams407@gmail.com>
2023-02-09 14:53:50 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+  
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
@@ -22,9 +23,7 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iure magni temporibus, repellat architecto molestiae a, iusto asperiores aliquam quam odio sint amet voluptatibus delectus, aspernatur dolor! Ipsum, enim error.</p>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
