07129102dc4121fa101b8801e846f025790bfc5c
= <=>
2023-02-16 22:32:41 +0200

    skills

--- src/components/about.vue
+++ src/components/about.vue
@@ -2,16 +2,54 @@
     <div class="about">
         <div class="container pt-5 pb-5">
             <div class="card mt-5">
-            <div class="row ms-5">
+                <div class="row ms-5">
                     <div class="col ms-5 mt-5 pe-5 pb-5">
                         <h1>About Me</h1>
-                        <p class="pt-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorum eligendi aut iure esse accusantium natus, a recusandae libero alias nam eum quibusdam ex laboriosam minima, illum ab eius repellendus odit temporibus nostrum animi tempore neque quas. Ipsum modi veritatis ad temporibus suscipit optio repudiandae vero molestias, officia delectus sit laudantium quod maxime velit dicta sunt repellat dolores. Obcaecati, ullam soluta? Consequuntur debitis ratione eligendi nostrum? Voluptatibus excepturi et earum? Earum dolores minima maiores, nobis, laboriosam minus explicabo aperiam pariatur, cum sapiente illum reiciendis? Laborum vero dicta quod totam aperiam? Nulla sequi quia consequatur qui velit atque delectus enim corrupti?
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ratione quas, voluptatum dolorum unde tempora in saepe? Cumque soluta ea quo iusto, modi, reprehenderit optio dolorum totam numquam dolore quaerat?</p>
+                        <p class="mt-4">
+                            Dedicated, present, focused and intuitive, is how people would 
+                            describe my personality.I am passionate and goal-driven about 
+                            anything I set my mind to. My aspirations are to become an striving
+                             frontend developer where my ideas and talent is recognized and seen.
+                        </p>
+                        <p class="pt-3">
+                            Often I find myself baking and cooking as a break from reality. I 
+                            enjoy doing practical work with my hands and listening to music as
+                             an escape. Personally I see myself as a curious person so adventures
+                            call my name, but I also believe that whatever is meant for me will 
+                            find me.
+                        </p>
+                        
                     </div>
                     <div class="col me-5" id="bg">
                         <div class="vl"></div>
                         <hr/>
                         <!-- <img src="https://i.postimg.cc/3NBfKTF2/Meagan-2.jpg"> -->
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-5 p-5">
+                <div class="row">
+                    <div class="col">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, ad ex quaerat eos sequi culpa, aliquid facere dicta dignissimos reprehenderit in. Explicabo ex, mollitia nam esse nostrum harum distinctio accusantium nulla iusto eveniet tenetur nisi. Pariatur nisi earum voluptatem optio cumque. Aliquam, ullam? Repudiandae, soluta? Accusantium officiis porro sit! Saepe temporibus praesentium tempore unde! Nam autem placeat architecto quae, quos perferendis nulla. Veritatis exercitationem ea debitis beatae itaque ad doloribus, maxime facere quis optio assumenda quibusdam aliquid voluptatum, sapiente libero ipsa sed deserunt dolorum? Dolorem incidunt deleniti quia placeat nisi consequuntur ut similique animi accusamus optio! Assumenda eaque quae natus?</p>
+                    </div>
+                    <div class="col">
+                        <h1>Skill Section</h1>
+                        <div class="row">
+                            <img src="https://i.postimg.cc/76v7LCMx/211925-social-wordpress-icon.png">
+                            <img src="https://i.postimg.cc/Y91FLpbQ/367627-html-html5-icon.png">
+                            <img src="https://i.postimg.cc/bNYndfb2/4691303-mysql-icon.png">
+                            <img src="https://i.postimg.cc/4N8992b2/7422531-css3-css-file-development-icon.png">
+                        </div>
+                        <div class="row">
+                            <img src="https://i.postimg.cc/GtdykZ5V/8546986-node-icon.png">
+                            <img src="https://i.postimg.cc/7h9TXKv6/8666135-bootstrap-icon.png">
+                            <img src="https://i.postimg.cc/44QtJxKg/8666273-figma-icon.png">
+                        </div>
+                        <div class="row">
+                            <img src="https://i.postimg.cc/6QZ2FWsF/9035136-logo-vue-icon.png">
+                            <img src="https://i.postimg.cc/fRpSH3Px/9035061-logo-javascript-icon.png">
+                            <img src="https://i.postimg.cc/1ty1GBy4/8666252-elementor-icon.png">
+                        </div>
                     </div>
                 </div>
             </div>
@@ -36,31 +74,40 @@
     background-size: cover;
 }
 p{
-    width:350px;
+    width:400px;
 }
 h1{
     
     font-size: 50px;
 }
-.vl{
+/* .vl{
     margin-top: 60%;
     margin-left:7%;
     height:300px;
     width:3px;
     background-color: rgba(157, 157, 157, 0.559);
+} */
+p{
+    font-size:17px;
 }
-hr{
+/* hr{
     margin-left:20%;
     margin-top: 30%;;
     width:300px;
     padding-top:3px;
     background-color: aliceblue;
-}
+} */
 .card{
-    background-color: rgba(0, 0, 0, 0);
+    background-color: rgba(237, 222, 222, 0);
     border-width:4px;
     border-color:grey;
     overflow: hidden;
+    height:550px;
+    /* margin-bottom:50px; */
 }
-
+img{
+    height:100px;
+    width:130px;
+    -webkit-filter: invert();
+}
 </style>
--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,22 +1,102 @@
 <template>
     <div class="contact">
-        <form action="https://formspree.io/f/xoqzeokl"
-       method="POST">
-       <label>
-        Email:
-        <input type="email" name="email" placeholder="eg: janedoe@gmail.com"/>
-       </label>
-       <label>
-        Message:
-        <textarea type="text" name="message" placeholder="Oh My Word your portfolio is amazing"></textarea>
-       </label>
-       <button type="submit">send</button>
-    </form>
+        <div class="card mx-auto">
+        <div class="container">
+            <div class="row">
+                    <div class="col mt-3">
+                        <h2>Contact Details</h2>
+                        <div class="row mt-4">
+                            <h3>
+                                Phone:
+                            </h3>
+                            <h5 class="ms-3">
+                                + 27 81 496 3622
+                            </h5>
+                        </div>
+                        <div class="row mt-3">
+                            <h3>
+                                CV:
+                            </h3>
+                            <a class="ms-3"
+                            href="https://drive.google.com/uc?export=download&id=1fyfdg9sg9hOnubwN9wKrxpiGA3ZgH4Er" 
+                            height="50px" 
+                            with="60px" 
+                            download>Download CV
+                        </a>
+                        </div>
+                        <div class="row mt-3">
+                            <h3>
+                                Email:
+                            </h3>
+                            <h5 class="ms-3">
+                                meaganwilliams407@gmail.com
+                            </h5>
+                        </div>
+                        <div class="row mt-3">
+                            <h3>
+                                Socials:
+                            </h3>
+                            <div class="row mx-auto" id="socials">
+                                <a href="https://github.com/maggi86">
+                                    <img src="https://i.postimg.cc/MGfVx0BP/211904-social-github-icon.png">
+                                </a>
+                                <a href="https://www.linkedin.com/in/meagan-williams-087920238/">
+                                    <img src="https://i.postimg.cc/FH7JgbTr/5282542-linkedin-network-social-network-linkedin-logo-icon.png">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="vr me-5 mt-5"></div>
+                    <div class="col">
+                        <form action="https://formspree.io/f/xoqzeokl"
+                        method="POST">
+                        <div class="row">
+                            <label class="pt-3">
+                                Fullname:
+                            </label>
+                            <input type="text" name="name" placeholder="eg: Jane Doe" required/>
+                        </div>
+                        <div class="row">
+                            <label class="pt-3">
+                                Email:
+                            </label>
+                            <input type="email" name="email" placeholder="eg: janedoe@gmail.com" required/>
+                        </div>
+                        <div class="row">
+                            <label class="pt-3">
+                                Message:
+                            </label>
+                            <textarea type="text" name="message" placeholder="Oh My Word your portfolio is amazing"></textarea>
+                        </div>
+                        <div class="row m-5">
+                            <button type="submit">send</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <style scoped>
 .contact{
    position:relative;
     bottom:0
+}
+.vr{
+    width:2px;
+    height:300px;
+    padding:0px;
+}
+.card{
+    height:700px;
+    width:100%;
+}
+#socials{
+    width:200px;
+}
+img{
+    height:50px;
+    width:50px;
 }
 </style>
